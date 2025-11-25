44c3f385315c8cc4c6968aad7f755d07c2ee8ea7
Jatin Sisodia <sisodia1999@gmail.com>
2025-11-25 15:57:09 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "#hoc/Navbar.jsx";
+
+const App = () => {
+    return (
+       <main>
+           <Navbar/>
+       </main>
+    )
+}
+export default App
